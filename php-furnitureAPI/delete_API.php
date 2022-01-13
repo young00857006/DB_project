a3169e7a9e1562de55ad2c93a904fe4293b84f40
young00857006 <tingyoungkuan@gmail.com>
2022-01-14 02:35:58 +0800

    後端API完成

--- php-furnitureAPI/delete_API.php
+++ php-furnitureAPI/delete_API.php
@@ -1,5 +1,5 @@
 <?php
-    require_once("login.php");
+    require_once("../login.php");
     $conn = new mysqli($hn, $un, $pw, $db);
     if ($conn->connect_error) die("Fatal Error");
 
---
+++ php-publisher/getPublisher_query.php
@@ -0,0 +1,32 @@
+<?php
+    require_once("../login.php");
+    $supId=$_GET["supId"];
+    $conn = new mysqli($hn, $un, $pw, $db);
+    
+    if ($conn->connect_error) die("Fatal Error");
+
+        $query = "SELECT * FROM publisher WHERE supId='".$supId."'";
+        $result = $conn->query($query);
+    if (!$result) die("Fatal Error");
+
+    
+    $rows = $result->num_rows;
+    
+    $arr = array();
+
+    for ($j = 0 ; $j < $rows ; ++$j)
+    {
+        $result->data_seek($j);
+        $row = $result->fetch_assoc();
+        array_push($arr,$row);
+    }
+    
+    //set encoding
+    header("content-Type: application/json; charset=utf-8");
+
+    //convert to json
+    echo json_encode($arr, JSON_UNESCAPED_UNICODE + JSON_PRETTY_PRINT);
+
+    $result->close();
+    $conn->close();
+?>
