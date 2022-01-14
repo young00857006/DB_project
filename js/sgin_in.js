3068741ddd6a2c9e94f3a2c55b7267a3ec407cce
zxc90836 <0979710326a@gmail.com>
2022-01-14 16:42:18 +0800

    前後端首次連接

--- js/sgin_in.js
+++ js/sgin_in.js
@@ -1,5 +1,5 @@
 $("document").ready(function(){
-    $("#sign_in_btn").click(function(){
+    /*$("#sign_in_btn").click(function(){
         var Merchant = "";
         sessionStorage.setItem('Merchant', "ssss");
         console.log("sss");
@@ -8,7 +8,7 @@ $("document").ready(function(){
 
         $.ajax({ 
             type: "POST",
-            url: "http://127.0.0.1:5000/DB/updateStatus", 
+            url: "php-member/Login.php", 
             data:sid,
             success: function(re){
                 //console.log(data);
@@ -26,7 +26,7 @@ $("document").ready(function(){
             }
         });
         
-    });
+    });*/
     $("#sign_up_btn").click(function(){
         var store = {};
         store.sId = $("#store_name").val();
